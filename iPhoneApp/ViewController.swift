b75b3036dd19648b601ed9ab722cfeecad0a1dac
Nithin Mahesh <nmahesh@constantcontact.com>
2016-08-09 16:39:39 -0400

    Initial Commit

---
+++ iPhoneApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPhoneApp
+//
+//  Created by Mahesh, Nithin on 8/9/16.
+//  Copyright © 2016 Mahesh, Nithin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
